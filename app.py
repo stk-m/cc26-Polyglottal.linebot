115b3a52d5436685d571dc1a4c604bee299a6406
stk-m <stk.murakami@gmail.com>
2022-05-17 12:16:13 +0900

    modify: make it better

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/")
-def hello():
-    return "hello, world"
+# @app.route("/")
+# def hello():
+#     return "hello, world"
 
 @app.route("/callback", methods=['POST'])
 def callback():
