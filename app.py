1566bd738732ab84246dc8e3892f0026e509b8cb
stk-m <stk.murakami@gmail.com>
2022-05-17 11:14:25 +0900

    modify envron

--- app.py
+++ app.py
@@ -12,42 +12,63 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
-#環境変数の呼び出し
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+# os.environ["YOUR_CHANNEL_ACCESS_TOKEN"] = "1t+iwyM7tk+YyGBU/vM/cZnhqrpR50pcwEN0lqCq3Qhu0Q0vBXvPSrcDm/Qv4NJWzgHc31noQKKnd5N1EtNtAlx+0y/qpa7naGwXEWyAdPfC4Vyg85l1RPCt6ch/MJlwUvunCyNgvtSpTVrhwaLfigdB04t89/1O/w1cDnyilFU="
+# os.environ["YOUR_CHANNEL_SECRET"] = "40562b809bf86841c2f90073df04edf9"
+
+
+#環境変数の呼び出し
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+print(os.environ)
+print(YOUR_CHANNEL_ACCESS_TOKEN)
+
+# YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
+# YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
+#print(YOUR_CHANNEL_ACCESS_TOKEN)
+# YOUR_CHANNEL_ACCESS_TOKEN = os.getenv("YOUR_CHANNEL_ACCESS_TOKEN")
+# YOUR_CHANNEL_SECRET = os.getenv("YOUR_CHANNEL_SECRET")
+
+# # print(os.environ)
+# print(YOUR_CHANNEL_ACCESS_TOKEN)
+# print(YOUR_CHANNEL_SECRET)
+
+# line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+# handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/")
 def hello():
     return "hello, world"
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         print("Invalid signature. Please check your channel access token/channel secret.")
+#         abort(400)
 
-    return 'OK'
+#     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
     app.run()
+
+# if __name__ == "__main__":
+#     port = int(os.getenv("PORT"))
+#     app.run(host="0.0.0.0", port=port)
