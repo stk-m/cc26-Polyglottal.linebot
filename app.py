d362b5d4b68fb08948a6ea931677a0fe7b960ff3
stk-m <stk.murakami@gmail.com>
2022-05-17 11:48:37 +0900

    feat: add .envand PORT

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, abort
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -20,8 +22,8 @@ app = Flask(__name__)
 #環境変数の呼び出し
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
-print(os.environ)
-print(YOUR_CHANNEL_ACCESS_TOKEN)
+# print(os.environ)
+#print(YOUR_CHANNEL_ACCESS_TOKEN)
 
 # YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
 # YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
@@ -33,42 +35,42 @@ print(YOUR_CHANNEL_ACCESS_TOKEN)
 # print(YOUR_CHANNEL_ACCESS_TOKEN)
 # print(YOUR_CHANNEL_SECRET)
 
-# line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-# handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/")
 def hello():
     return "hello, world"
 
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         print("Invalid signature. Please check your channel access token/channel secret.")
-#         abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-#     return 'OK'
-
-
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=event.message.text))
+    return 'OK'
 
 
-if __name__ == "__main__":
-    app.run()
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 # if __name__ == "__main__":
-#     port = int(os.getenv("PORT"))
-#     app.run(host="0.0.0.0", port=port)
+#     app.run(debug=True, port=8080)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
