66fb571f48f200f2f11d7c268d144f331781e61f
stk-m <stk.murakami@gmail.com>
2022-05-18 16:43:06 +0900

    refactor: clear the comment and add README

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # LINE bot
-くまのすけ(teddy bear boy)
+くまのすけ(Kumanosuke) <br>
+    He is a teddy bear boy who can talk with you through the Line app. <br>
+    He likes to communicate but his human language is little bit weird!
 
-## Heroku
+<img src="./image/linebotxigpm.png">
+
+## about the app
+### set up
+- read the QR code and add friend in your Line account
+
+- talk with him!
+    - if you send a message, he replies randomly
+    - if you send "あそぼ", he send the button and you can choose the link
+    - if you send sticker, he also send you sticker randomly
+
+### Heroku
 https://cc26-polyglottal-linebot.herokuapp.com/
-(just you can see the comment)
+(used as server)
--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, abort
 import os
 import random
-"""
-# from dotenv import load_dotenv
-# load_dotenv()
-"""
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
-print()
+
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    # MessageEvent, TextMessage, TextSendMessage
     MessageEvent, TextMessage, TextSendMessage, StickerMessage, StickerSendMessage,
     TemplateSendMessage, ButtonsTemplate, PostbackAction
 )
@@ -48,33 +44,6 @@ def callback():
         abort(400)
 
     return 'OK'
-
-
-# #@handler.add(MessageEvent, message=TextMessage)
-
-# # def handle_message(event):
-# #     line_bot_api.reply_message(
-# #         event.reply_token,
-# #         TextSendMessage(text=event.message.text))
-
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     comment_list = [
-#         "Hello, I'm Kumanosuke!", 
-#         "Have you eaten Cannelétte? You should! https://www.uha-mikakuto.co.jp/catalog/other/ot002.html",
-#         "aaaaa~~~~~",
-#         "nemui...",
-#         "bunny ear!()()",
-#         "Kuma! Kuma! Kuma~~~~!",
-#         "Bonjour!",
-#         "coffee or tea?", 
-#         "Let's go home~~ https://www.youtube.com/watch?v=dUXeJTzSCjc"
-#     ]
-#     comment_index = random.randint(0, 8)
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=comment_list[comment_index])
-#     )
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -147,10 +116,3 @@ def handle_sticker_message(event):
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
-"""
-# if __name__ == "__main__":
-    # port = int(os.getenv("PORT", 8080))
-    # app.run(host="0.0.0.0", port=port)
-    # app.run(port=os.environ.get('PORT', 8080))
-    # app.run()
-"""
